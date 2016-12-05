06e0a778ea0e05ede2e945ab83606a1d557b96f3
Matthew Finerty <evilarceus@gmail.com>
2016-12-05 08:34:37 -0800

    Added traffic task

---
+++ Traffic.c
@@ -0,0 +1,22 @@
+
+/*
+  Project Title: Traffic situation
+  Team Members: Matthew Finerty
+  Date: November 9th, 2016
+  Section: Period 4
+
+
+  Task Description:
+
+
+  Pseudocode:
+
+*/
+
+task main()
+{                                     //Program begins, insert code within curly braces
+
+
+
+
+}
